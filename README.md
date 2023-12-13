6fd39587a9c7b604e0c22cd2df5c82e2747096f1
Tsim <tyang4204@gmail.com>
2023-12-13 12:25:06 -0800

    finished up a portion of the code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 This is the README.md file.
+This is a second line to test my code.
--- main.c
+++ main.c
@@ -7,11 +7,33 @@
  */
 int main(int argc, char **argv)
 {
+  FILE *document;
+  char *line = NULL;
+  size_t size_max = 32, characters;
   if (argc != 2)
     {
       printf("USAGE: monty file\n");
       exit(EXIT_FAILURE);
     }
-  printf("%s %s\n", argv[0], argv[1]);
+  
+  document = fopen(argv[1], "r");
+  if (document == NULL)
+    {
+      printf("Error: Can't open %s\n", argv[1]);
+      exit(EXIT_FAILURE);
+    }
+  
+  characters = getline(&line, &size_max, document);
+  if (!characters)
+    {
+      printf("something wrong with the text\n");
+      exit(EXIT_FAILURE);
+    }
+  
+  printf("%s\n", line);
   return (0);
 }
+
+char *token(char *string)
+{
+  
